728afe9ae467eb438818e47b142048d250a55692
22asad <86917288+22asad@users.noreply.github.com>
2023-01-13 13:22:20 -0800

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,22 @@
+<div class="container my-6 bg-dark text-light my-10">
+    <div class="row">
+        <div class="col-lg-4">
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque velit odit, officiis iste harum aut distinctio neque a dolore fuga fugit labore! Dolor tempora, deserunt dolores facilis illum ab provident!</p>
+        </div>
+        <div class="col-lg-4">
+            <h2>Services</h2>
+            <a href="#">This is a link</a><br>
+            <a href="#">This is a link</a><br>
+            <a href="#">This is a link</a><br>
+            <a href="#">This is a link</a><br>
+        </div>
+        <div class="col-lg-4">
+            <h2>Contact Us</h2>
+            <a href="#">This is a link</a><br>
+            <a href="#">This is a link</a><br>
+            <a href="#">This is a link</a><br>
+            <a href="#">This is a link</a><br>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php require('inc/links.php') ?>
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <?php require('inc/header.php') ?>
+
+                                     <!-- Crousel  -->
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+        aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+        aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://source.unsplash.com/1500x300/?technology,hardware" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+          <button type="button" class="btn btn-primary"> full Home Automation</button>
+          <button type="button" class="btn btn-success">Single Room Automation</button>
+          <button type="button" class="btn btn-danger">Primary</button>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="https://source.unsplash.com/1500x300/?technology,coding" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+          <button type="button" class="btn btn-primary"> full Home Automation</button>
+          <button type="button" class="btn btn-success">Single Room Automation</button>
+          <button type="button" class="btn btn-danger">Primary</button>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="https://source.unsplash.com/1500x300/?nature,water" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+          <button type="button" class="btn btn-primary"> full Home Automation</button>
+          <button type="button" class="btn btn-success">Single Room Automation</button>
+          <button type="button" class="btn btn-danger">Primary</button>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+                                 <!-- about section  -->
+
+  <div class="container my-4">
+    <h1 class="text-center">About Us</h1>
+    <p class="text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas doloremque soluta autem, assumenda rerum
+      suscipit placeat voluptates a nisi reprehenderit.
+    </p>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card h-100">
+          <img src="images/bg.jpeg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+              content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col asa">
+        <div class="card h-100">
+          <img src="https://source.unsplash.com/500x300/?nature,water" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a short card.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="https://source.unsplash.com/500x300/?nature,water" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+              content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="https://source.unsplash.com/500x300/?nature,water" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+              content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card h-100">
+          <img src="https://source.unsplash.com/500x300/?nature,water" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+              content. This content is a little bit longer.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+    <div class="container my-4">
+      <h1 class="text-center" >Our Services</h1>
+      <p class="text-center" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ipsa vitae quo consequatur, aspernatur magni, esse recusandae nostrum blanditiis atque vero architecto totam maiores dolor iure iste quam porro cumque.</p>
+      <div class="row my-5 ">
+        <div class="col-lg-6 border p-0 ">
+          <img src="https://source.unsplash.com/640x300/?nature,water" alt="">
+        </div>
+        <div class="col-lg-6 text-center border p-3">
+          <h1>This is a heading</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime fuga temporibus velit qui cupiditate, neque rem laborum quisquam nemo repellendus? Aspernatur, animi? Ex, eligendi neque id reprehenderit quod dignissimos quo alias eius accusantium ea excepturi repudiandae enim corrupti consectetur suscipit?</p>
+          <button type="button" class="btn btn-danger">Primary</button>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+    <?php require('inc/footer.php'); ?>
+</body>
+
+</html>
